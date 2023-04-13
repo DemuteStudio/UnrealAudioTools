5e005ed5d384bd459affd11d6860c39f85b6539c
Pablo - Demute <peschwildendiaz@hotmail.com>
2023-04-13 19:58:52 +0200

    First implementation of static mesh emitters

---
+++ Source/DM_StaticMeshEmitters/Private/DM_StaticMeshEmitters.cpp
@@ -0,0 +1,17 @@
+﻿#include "DM_StaticMeshEmitters.h"
+
+#define LOCTEXT_NAMESPACE "FDM_StaticMeshEmittersModule"
+
+void FDM_StaticMeshEmittersModule::StartupModule()
+{
+    
+}
+
+void FDM_StaticMeshEmittersModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDM_StaticMeshEmittersModule, DM_StaticMeshEmitters)
---
+++ Source/DM_StaticMeshEmitters/Private/StaticMeshAudioEmitterBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaticMeshAudioEmitterBase.h"
+
+#include "StaticMeshEmitterSourceComponent.h"
+
+// Sets default values
+AStaticMeshAudioEmitterBase::AStaticMeshAudioEmitterBase(const class FObjectInitializer& ObjectInitializer) : Super (ObjectInitializer)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SourceComponent = ObjectInitializer.CreateDefaultSubobject<UStaticMeshEmitterSourceComponent>(this, TEXT("SourceComponent"));
+}
+
+// Called when the game starts or when spawned
+void AStaticMeshAudioEmitterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStaticMeshAudioEmitterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
